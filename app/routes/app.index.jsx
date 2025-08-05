f719bb1784532f521be3958d580225156a1f62d7
anasbusytech <anas24.busytech@gmail.com>
2025-08-05 15:15:02 +0500

    final update

--- app/routes/app.index.jsx
+++ app/routes/app.index.jsx
@@ -7,7 +7,5 @@ app.get('/', (req, res) => {
   res.send('Hello, Shopify App!');
 });
 
-// Make sure your server listens on the default port
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+// Export the handler for Vercel
+module.exports = app;
